af17388a64a64944daa0add0445c02900ec2a8a1
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-08-18 22:34:59 +0530

    changes in payment

--- src/app/static/app/js/payment.js
+++ src/app/static/app/js/payment.js
@@ -6,10 +6,19 @@ else if (window.screen.width <= 400) {
     document.querySelector(".payment").style.display = 'none'
 }
 
-// Session storage
+// Continue to payment button
 document.getElementById('paypal').addEventListener('click', () => {
-    document.getElementById('continue').style.display = 'flex'
+    document.getElementById('continue').style.display = 'flex';
 });
+
+document.getElementById('paypal-mob').addEventListener('click', () => {
+    document.getElementById('next-mob').style.display = 'flex';
+});
+
+let next = document.querySelectorAll('#next-mob')
+
+
+
 
 window.onload = () => {
     alert("Please select paypal option to proceed")
